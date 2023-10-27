1661e392b5db292cd775f4ab6944f3d8f82971e8
Ruth Mores <moresruthantonette@gmail.com>
2023-10-27 15:52:13 +0800

    homepage

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum,
+        excepturi amet quibusdam maxime tenetur veritatis temporibus eius? Quod,
+        nobis eaque.
+      </p>
+    </div>
+  );
 };
 
 export default Home;
